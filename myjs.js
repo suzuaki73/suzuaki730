dc71593b19212d156438684f3c9f01f8a26ed555
suzuaki73 <zm25191@josai.ac.jp>
2025-07-15 10:02:08 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,8 +1,8 @@
 alert('Hello,Akito!');
 alert(1+1);
 alert(math.sqrt(1+1));
-alert(2+3)
-alert('2'+'3')
-alert(Number('2')+Nunber('3'))
-alert('2'+3)
-alert('2')+3)
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Nunber('3'));
+alert('2'+3);
+alert('2')+3);
